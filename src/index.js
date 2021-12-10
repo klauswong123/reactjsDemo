e5e9a6d29a14f8d29069cd2774588c7fe52ad121
wongkl2 <wongkl2@oocl.com>
2021-12-10 19:00:06 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+
+// Importing combination
+import React, {Component} from 'react';
+// Importing Module
 import ReactDOM from 'react-dom';
+import Demo from './demo.js';
+// Importing CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+ 
+class App extends Component {
+  render()
+  {
+      return (<div>
+      <Demo title="GeeksforGeeks" /></div>);
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<App/>,
+document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
